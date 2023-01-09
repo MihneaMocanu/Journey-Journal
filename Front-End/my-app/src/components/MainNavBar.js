8a619ff7ca1a3299f64ae4ae6cd83ba5b537e8fb
chrisneagu <chris.neagu@outlook.com>
2023-01-10 01:22:28 +0200

    Trying new commit

---
+++ Front-End/my-app/src/components/MainNavBar.js
@@ -0,0 +1,36 @@
+import "./MainNavBar.css"
+import { Link } from "react-router-dom";
+
+const MainNavBar = () => {
+    return (
+            <header>
+                <div className="container">
+                    <nav>
+                        <Link to="/" className="logo">
+                            <h2> <span>J</span>ourney<span>J</span>ournal</h2>
+                            
+                        </Link>
+                        <ul>
+                            <li> 
+                                <Link to="/public" className="nav-link">
+                                    Experiences
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/private" className="nav-link">
+                                    My Experiences
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/login" className="nav-link">
+                                    Login
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+    );
+};
+
+export default MainNavBar;
