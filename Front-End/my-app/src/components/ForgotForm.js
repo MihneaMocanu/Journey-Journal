3b4459e7924dec9bfc6f99ba0c29c9b725cf90bd
chrisneagu <chris.neagu@outlook.com>
2023-01-18 13:52:54 +0200

    Login done

--- Front-End/my-app/src/components/ForgotForm.js
+++ Front-End/my-app/src/components/ForgotForm.js
@@ -6,7 +6,7 @@ import { SERVER_URL } from './constants';
 import { ToastContainer, toast } from 'react-toastify';
 import store from "../store/store";
 import 'react-toastify/dist/ReactToastify.css';
-import { SignJWT } from 'jose';
+import { useNavigate } from 'react-router-dom';
 emailjs.init("service_q40254f");
 
 
@@ -15,14 +15,13 @@ function rand(){
 }
 
 const sendEmail = (email, token) => {
-
+    console.log(token);
     const message = {
         userEmail: email,
         subject: 'Reset Password Token',
-        text: `Your token is: ${token} and will expire in 10 minutes`,
+        text: `Hi, your token is: ${token} and will expire in 10 minutes`,
         from: 'JourneyJournal',
     };
-
     emailjs.send("service_q40254f", 'template_abaz6ub', message,'wu6NVoCH_lkHMX-bw')
         .then(() => {
             console.log('email sent');
@@ -38,17 +37,125 @@ const sendEmail = (email, token) => {
 }
 
 function ForgotForm() {
-  
+  const [user, setUser] = useState({})
   const [email, setEmail] = useState('');
-  const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [retypedPassword, setRetypedPassword] = useState('');
   const [sent, setSent] = useState(false);
   const [token, setToken] = useState('')
   const [tokenReceived, setTokenReceived] = useState('')
 
+  const navigate = useNavigate();
+
   async function handleReset(event){
     event.preventDefault();
+
+    console.log(token)
+
+    if(tokenReceived === ' '){
+      toast.error("Empty token field", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem"
+        }
+      });
+      return;
+    }
+
+    if(tokenReceived !== token){
+      toast.error("Invalid token", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem"
+        }
+      });
+      return;
+    }
+
+    if(newPassword === ''){
+      toast.error("Empty new password", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem",
+          fontSize: "1.2rem"
+        }
+      });
+      return;
+    }
+
+    if(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/i.test(newPassword) === false){
+      toast.error("Password must include at least one upper case, one lower, one digit, one special character", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+        style: {
+          marginTop: "5rem"
+        }
+      });
+      return;
+    }
+
+    if(retypedPassword === ''){
+      toast.error("Empty retyped password", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem",
+          fontSize: "1.2rem"
+        }
+      });
+      return;
+    }
+
+   
+    if(user.password === newPassword){
+      toast.error("The new password is the same as the old one", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem",
+          fontSize: "1.2rem"
+        }
+      });
+      return;
+    }
+
+    if(retypedPassword !== newPassword){
+      toast.error("Passwords do not match", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem",
+          fontSize: "1.2rem"
+        }
+      });
+      return;
+    }
+
+    user.password = newPassword;
+    const res = await fetch(`${SERVER_URL}/users/${user.id}`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    
+    if(res.status === 200)
+    {
+      toast.success("Succesfully reseted your password!", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 1000,
+        style: {
+          marginTop: "5rem",
+        }
+      });
+      setTimeout(() => {
+          navigate('/login');
+      }, 2000);
+    }
   }
   async function handleSubmit(event) {
     event.preventDefault();
@@ -76,13 +183,14 @@ function ForgotForm() {
         return;
     }
     
-    const user = await resEmail.json();
-    setToken(rand() + rand());
+    setUser(await resEmail.json());
+    let aux = rand() + rand();
+    setToken(aux);
     const timeout = 10 * 60 * 1000;
     setTimeout(() => {
        setToken('');
     }, timeout);
-    //sendEmail(email, token);
+    sendEmail(email, aux);
     setSent(true);
   }
 
@@ -113,15 +221,6 @@ function ForgotForm() {
               />
             </label>
             <label>
-            Current Password:
-            <input className="input-login" 
-              type="password"
-              placeholder="Enter your current password" 
-              value={currentPassword}
-              onChange={event => setCurrentPassword(event.target.value)}
-            />
-          </label>
-            <label>
             New Password:
             <input className="input-login" 
               type="password"
@@ -131,12 +230,12 @@ function ForgotForm() {
             />
           </label>
             <label>
-            Current Password:
+            Retyped Password:
             <input className="input-login" 
               type="password"
               placeholder="Enter your current password" 
-              value={currentPassword}
-              onChange={event => setCurrentPassword(event.target.value)}
+              value={retypedPassword}
+              onChange={event => setRetypedPassword(event.target.value)}
             />
           </label> 
           <button type="submit" className="button" onClick={handleReset}>Reset</button>
